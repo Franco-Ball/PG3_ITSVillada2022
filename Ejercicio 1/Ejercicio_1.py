2dddabb84d46f1316726e1b3766caa681abd0260
FrancoBall <francoball181@gmail.com>
2022-04-01 08:22:28 -0300

    Correccion Ejericio_1

--- Ejercicio 1/Ejercicio_1.py
+++ Ejercicio 1/Ejercicio_1.py
@@ -1,13 +1,15 @@
-List = [14,8,3,6,2,9,1,4,15,11,7,10,5,12,13]
+List: int = [14, 8, 3, 6, 2, 9, 1, 4, 15, 11, 7, 10, 5, 12, 13]
 
-def Get_list_index(List, number):
+
+def Get_list_index(List: list, number: int) -> None:
     for x in List:
         if x == number:
-            index = List.index(number) 
-            print("El numero que buscaste esta en el indice: " + str(index))
+            index: int = List.index(number)
+            print(f"El numero que buscaste esta en el indice: " + {index})
+
 
 print("La lista contiene numeros del 1 al 15!")
 print("Ingrese el numero que quiere buscar en la lista para obtener su indice")
-num = int(input(""))
+num: int = int(input(""))
 
 Get_list_index(List, num)
