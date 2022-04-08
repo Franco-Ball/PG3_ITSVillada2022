4373390e23a48cb56be129c96a47573d625385a3
FrancoBall <francoball181@gmail.com>
2022-04-08 09:49:28 -0300

    Desafio Terminado

--- Ejercicio 1/Ejercicio_3.py
+++ Ejercicio 1/Ejercicio_3.py
@@ -1,14 +1,23 @@
 def print_equilateral_triangle() -> None:
     n = int(input("Ingrese el alto del triangulo: "))
     char = input("Ingrese el caracter con el que se representara el triangulo: ")
-    for i in range(n):
-        print(" " * (n - i - 1) + (char + " ") * (i + 1))
+    if n >= 1:
+        for i in range(n):
+            print(" " * (n - i - 1) + (char + " ") * (i + 1))
+    elif n == 0:
+        print("No se puede imprimir un triangulo de alto 0")
+    else:
+        n = abs(n)
+        for i in range(n):
+            print(" " * i + (char + " ") * (n - i))
 
 
 def print_rectangle() -> None:
     ancho: int = int(input("Ingrese el ancho del rectangulo: "))
     alto: int = int(input("Ingrese el alto del rectangulo: "))
-    carac: str = str(input("Ingrese el caracter con el que se representara el rectangulo: "))
+    carac: str = str(
+        input("Ingrese el caracter con el que se representara el rectangulo: ")
+    )
 
     for x in range(alto):
         print(ancho * carac)
