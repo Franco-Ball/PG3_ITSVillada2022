2cbd49d8e7f7cbcb95213e61fa3de725621c52c5
Franco-Ball <francoball181@gmail.com>
2022-04-29 11:37:47 -0300

    Realizado Ejercicio 2

---
+++ Ejercicios_Manejo_Exceptions/Ejercicio_2.py
@@ -0,0 +1,18 @@
+from asyncio import as_completed
+
+
+while True:
+    try:
+        x = int(input("Ingrese un numero: "))
+        y = int(input("Ingrese otro numero: "))
+
+        print("La division es: ", x / y)
+        break
+    except ValueError:
+        print(
+            "Error, no ingreso un numero, porfavor ingrese dos numero para poder realizar su division"
+        )
+    except ZeroDivisionError:
+        print(
+            "Error, no se puede dividir por cero, porfavor ingrese dos numero diferentes de cero para poder realizar su division"
+        )
