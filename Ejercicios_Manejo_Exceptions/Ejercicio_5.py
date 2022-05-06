b25f3435c4278dc3e765845159ac2f234d3ddfce
Franco-Ball <francoball181@gmail.com>
2022-05-06 08:38:49 -0300

    Ejercicio 5 correccion

--- Ejercicios_Manejo_Exceptions/Ejercicio_5.py
+++ Ejercicios_Manejo_Exceptions/Ejercicio_5.py
@@ -1,23 +1,22 @@
 try:
 
-        f = open("Ejercicios_Manejo_Exceptions/Texto_Ejercicio_5.txt", "w")
-        f.write("Bienvenidos al documento de text del Ejercicio 5! \n")
-        contenido = input("Ingrese el contenido que quiere agregale al documento: ")
-        
-        
-        chars = set("0123456789")
+    f = open("Ejercicios_Manejo_Exceptions/Texto_Ejercicio_5.txt", "w")
+    f.write("Bienvenidos al documento de text del Ejercicio 5! \n")
+    contenido = input("Ingrese el contenido que quiere agregale al documento: ")
 
-        if all((c in chars) for c in contenido):
-            f.write(int(contenido))
-        else:
-            f.write(contenido)
+    chars = set("0123456789")
 
-        f.close() 
+    if all((c in chars) for c in contenido):
+        f.write(int(contenido))
+    else:
+        f.write(contenido)
 
-        print("Ahora veamos como se ve el documento:")
+    f.close()
 
-        f = open("Ejercicios_Manejo_Exceptions/Texto_Ejercicio_5.txt", "r")
-        print(f.read())
-        f.close()
+    print("Ahora veamos como se ve el documento:")
+
+    f = open("Ejercicios_Manejo_Exceptions/Texto_Ejercicio_5.txt", "r")
+    print(f.read())
+    f.close()
 except TypeError:
     print("No se puede ingresar enteros en el metodo write!")
