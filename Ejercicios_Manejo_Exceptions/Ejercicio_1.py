4fd643fe0f048e0d510a01179d1ccedd1004c2d1
Franco-Ball <francoball181@gmail.com>
2022-05-06 09:58:53 -0300

    Correccion Ejercicio 1

--- Ejercicios_Manejo_Exceptions/Ejercicio_1.py
+++ Ejercicios_Manejo_Exceptions/Ejercicio_1.py
@@ -4,8 +4,15 @@ while True:
         y = int(input("Ingrese otro numero: "))
 
         print("La suma es: ", x + y)
-        break
+        
+        while True:
+            flag = input("¿Desea seguir sumando valores? (S/N) ")
+            if flag == "S":
+                continue
+            elif flag == "N":
+                exit()
+            else:
+                print("Ingrese una opcion valida!")
+                
     except ValueError:
-        print(
-            "Error, no ingreso un numero, porfavor ingrese dos numero para poder realizar su suma"
-        )
+        print("Error, no ingreso un numero, porfavor ingrese dos numero para poder realizar su suma")
